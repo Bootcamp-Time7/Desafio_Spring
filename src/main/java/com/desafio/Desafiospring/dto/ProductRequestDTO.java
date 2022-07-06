7637330c5093b37419efe2d14ba3781267dca9f4
amandaamabili <amanda.amabili@mercadolivre.com>
2022-07-06 14:32:08 -0300

    Criado estrutura de pasta

---
+++ src/main/java/com/desafio/Desafiospring/dto/ProductRequestDTO.java
@@ -0,0 +1,29 @@
+package com.desafio.Desafiospring.dto;
+
+import com.desafio.Desafiospring.model.Product;
+import lombok.Data;
+
+@Data
+public class ProductRequestDTO {
+    private long productId;
+    private String name;
+    private String category;
+    private String brand;
+    private double price;
+    private int quantity;
+    private boolean freeShipping;
+    private String prestige;
+
+    public ProductRequestDTO(Product product) {
+        this.productId = product.getProductId();
+        this.name = product.getName();
+        this.category = product.getCategory();
+        this.brand = product.getBrand();
+        this.price = product.getPrice();
+        this.quantity = product.getQuantity();
+        this.freeShipping = product.isFreeShipping();
+        this.prestige = product.getPrestige();
+    }
+
+
+}
---
+++ src/main/java/com/desafio/Desafiospring/repository/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.desafio.Desafiospring.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepo {
+
+
+}
